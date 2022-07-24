681e886667a0d3c9b3efbfc2a86e2f0ce3cfc04f
MdRoki2020 <mroki815@gmail.com>
2022-07-24 23:33:38 +0600

    simple compelete api

--- my_api/app.js
+++ my_api/app.js
@@ -1,7 +1,49 @@
 const express=require('express');
 const router=require("./src/routes/Api");
-
 const app=new express();
+
+//security Middleware import
+const rateLimit= require('express-rate-limit');
+const helmet= require('helmet');
+const mongoSanitize= require('express-mongo-sanitize');
+const xss= require('xss-clean');
+const hpp= require('hpp');
+const cors= require('cors');
+
+
+//security middleware impliment
+app.use(helmet())
+app.use(mongoSanitize())
+app.use(xss())
+app.use(hpp())
+app.use(cors())
+
+
+//Request Rate Limiting
+const limiter=rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+})
+
+app.use(limiter);
+
+
+
+
+
+
 app.use("/api/v1",router);
+
+
+
+
+
+
+
+
+//undefined Route
+app.use('*',(req,res)=>{
+    res.status(404).json({status:"failed",data:"Not Found"})
+})
 
 module.exports=app;
--- my_api/index.js
+++ my_api/index.js
@@ -1,5 +1,8 @@
 const app=require('./app');
+const dotenv=require("dotenv");
 
-app.listen(5000,()=>{
-    console.log('Success')
+dotenv.config({path: './config.env'})
+
+app.listen(process.env.RUNNING_PORT,()=>{
+    console.log('Our Port Is Successfully Running At '+process.env.RUNNING_PORT);
 });
